0001010d598654f8fbbed2bd67078c514b5fc61c
oliviadugger <oliviacdugger@gmail.com>
2018-07-13 18:15:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 # dog.rb
 class Dog
-  attr_reader :name
+  attr_reader :name, :breed
+  attr_writer :breed
+  
   def name=(dog_name)
    @name = dog_name
   end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
 end
 fido = Dog.new
 
